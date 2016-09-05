4ce7e8daf0134b59ddc285daf60bd7fbccaeacb8
Jeremy Klein <kleinjj@lafayette.edu>
2016-09-05 12:19:12 -0400

    Image selected

--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -11,21 +11,36 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //set image picker delegate
+        imagePicker.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
     @IBAction func pickAnImage(sender: AnyObject){
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
+    //Implemented when Image is selected
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // Check if image is selected
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            //set image
+            imagePickerView.image = pickedImage
+            //Scale image view
+            imagePickerView.contentMode = .ScaleAspectFit
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    //Used by Cancel button on UIImagePickerController
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
 }
 
