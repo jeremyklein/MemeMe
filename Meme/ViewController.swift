06a8578368c2ad47531bd9ee28fb113b8f63a7f5
Jeremy Klein <kleinjj@lafayette.edu>
2016-09-13 06:18:54 -0400

    Added separate Camera and Library buttons. Camera button is disabled when camera source is unavailable

--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -13,16 +13,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imagePickerView: UIImageView!
     let imagePicker = UIImagePickerController()
     
+    @IBOutlet weak var CameraButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         //set image picker delegate
         imagePicker.delegate = self
+        
+    }
+    override func viewWillAppear(animated:Bool) {
+    super.viewWillAppear(true)
+        CameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
     }
     
 
-    @IBAction func pickAnImage(sender: AnyObject){
-        self.presentViewController(imagePicker, animated: true, completion: nil)
+    
+    @IBAction func pickAnImageFromLibrary(sender:AnyObject){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated:true, completion:nil)
+        
     }
+    
+    @IBAction func pickAnImageFromCamera(sender:AnyObject){
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePicker, animated:true, completion:nil)
+        
+    }
+    
     //Implemented when Image is selected
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         // Check if image is selected
